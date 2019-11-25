58eb8375415a74dfc558cd78c47f944dffe8da5b
Krytosys <47263260+Krytosys@users.noreply.github.com>
2019-11-25 19:19:52 +0800

    Signup(fixed) + database

--- app/src/main/java/com/example/flyingpotato/SignUpFragment.java
+++ app/src/main/java/com/example/flyingpotato/SignUpFragment.java
@@ -2,21 +2,30 @@ package com.example.flyingpotato;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpFragment extends Fragment {
 
     private View view;
 
     private Button signup, back;
+    private EditText username, password;
+
+    private DatabaseReference register;
 
     public SignUpFragment(){
 
@@ -31,19 +40,27 @@ public class SignUpFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        register = FirebaseDatabase.getInstance().getReference("Users");
+
+
         view = inflater.inflate(R.layout.fragment_signup, container, false);
 
         signup = view.findViewById(R.id.signup);
         back = view.findViewById(R.id.back);
 
+        username = view.findViewById(R.id.ETusername);
+        password = view.findViewById(R.id.ETpassword);
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
+                addUser();
                 Intent intent = new Intent(getActivity(), MainActivity.class);
                 startActivity(intent);
                 getActivity().overridePendingTransition(0, 0);
                 getActivity().finish();
-            }
+
+
+        }
         });
 
         back.setOnClickListener(new View.OnClickListener() {
@@ -66,4 +83,16 @@ public class SignUpFragment extends Fragment {
         ft.replace(R.id.frame_container, fragment, tag);
         ft.commitAllowingStateLoss();
     }
+    public void addUser() {
+        String name = username.getText().toString().trim();
+        String passw = password.getText().toString().trim();
+        if(!TextUtils.isEmpty(name)) {
+            String id = register.push().getKey();
+            Users user = new Users(id, name, passw);
+            register.child(id).setValue(user);
+            Toast.makeText(getActivity(), "User signup success!", Toast.LENGTH_LONG).show();
+        }else
+            Toast.makeText(getActivity(),"Please fill all the blanks!", Toast.LENGTH_LONG).show();
+    }
+
 }
--- app/src/main/java/com/example/flyingpotato/Users.java
+++ app/src/main/java/com/example/flyingpotato/Users.java
@@ -1,4 +1,69 @@
 package com.example.flyingpotato;
 
 public class Users {
+    String name;
+    String password;
+    String id;
+    int powerup1;
+    int powerup2;
+    int powerup3;
+    double cash;
+
+    public Users(String id,String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        powerup1 = 0;
+        powerup2 = 0;
+        powerup3 =0;
+        cash = 0.0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPowerup1() {
+        return powerup1;
+    }
+
+    public void setPowerup1(int powerup1) {
+        this.powerup1 = powerup1;
+    }
+
+    public int getPowerup2() {
+        return powerup2;
+    }
+
+    public void setPowerup2(int powerup2) {
+        this.powerup2 = powerup2;
+    }
+
+    public int getPowerup3() {
+        return powerup3;
+    }
+
+    public void setPowerup3(int powerup3) {
+        this.powerup3 = powerup3;
+    }
+
+    public double getCash() {
+        return cash;
+    }
+
+    public void setCash(double cash) {
+        this.cash = cash;
+    }
 }
