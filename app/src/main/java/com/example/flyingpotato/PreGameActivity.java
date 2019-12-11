adba15d65fcc843e4a3174272151d2b3627e3e64
Secade <earl_capistrano@dlsu.edu.ph>
2019-12-11 22:38:56 +0800

    Added power ups

--- app/src/main/java/com/example/flyingpotato/PreGameActivity.java
+++ app/src/main/java/com/example/flyingpotato/PreGameActivity.java
@@ -1,28 +1,55 @@
 package com.example.flyingpotato;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PreGameActivity extends AppCompatActivity {
 
     SharedPreferences pref;
     LinearLayout thingie;
+    Spinner spinner;
+    private List<Users> users;
+    private Users user;
+    private DatabaseReference database;
+    private int selectedPotion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        selectedPotion=0;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_game);
         getSupportActionBar().hide();
 
+        pref = getSharedPreferences("user_details", MODE_PRIVATE);
+        users = new ArrayList<>();
+
+        database = FirebaseDatabase.getInstance().getReference("Users");
+
         thingie = findViewById(R.id.thingie);
         pref = getSharedPreferences("user_details", MODE_PRIVATE);
+        spinner = findViewById(R.id.spinner);
 
         if(!pref.contains("username")){
             thingie.removeAllViews();
@@ -33,6 +60,64 @@ public class PreGameActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                users.clear();
+
+                for (DataSnapshot data: dataSnapshot.getChildren()) {
+                    user = data.getValue(Users.class);
+                    users.add(user);
+                }
+                for(Users user: users){
+                    if(user.getName().compareTo(pref.getString("username", "")) == 0){
+                        List<String> list = new ArrayList<String>();
+                        if (user.getLowSpeed()>0)
+                            list.add("Slow Mo Potion x" + user.getLowSpeed());
+                        if (user.getGoldMulti()>0)
+                            list.add("Money Multiplier Potion x"+user.getGoldMulti());
+                        if (user.getLessObs()>0)
+                            list.add("Obstruction Potion x"+user.getLessObs());
+                        if (user.getLowSpeed()==0&&user.getGoldMulti()==0&&user.getLessObs()==0)
+                            list.add("No Power Ups!");
+                        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, list);
+                        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        spinner.setAdapter(dataAdapter);
+                        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                            @Override
+                            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                                if (adapterView.getItemAtPosition(i).toString().contains("Slow Mo")){
+                                    selectedPotion=1;
+                                    Toast.makeText(getApplicationContext(), "Slow Mo Potion Selected!", Toast.LENGTH_LONG).show();
+                                }else if(adapterView.getItemAtPosition(i).toString().contains("Money Multiplier")){
+                                    selectedPotion=2;
+                                    Toast.makeText(getApplicationContext(), "Money Multiplier Potion Selected!", Toast.LENGTH_LONG).show();
+                                }else if(adapterView.getItemAtPosition(i).toString().contains("Obstruction")){
+                                    selectedPotion=3;
+                                    Toast.makeText(getApplicationContext(), "Obstruction Potion Selected!", Toast.LENGTH_LONG).show();
+                                }
+                            }
+
+                            @Override
+                            public void onNothingSelected(AdapterView<?> adapterView) {
+
+                            }
+                        });
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
     public void back(View view){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
@@ -41,6 +126,8 @@ public class PreGameActivity extends AppCompatActivity {
     }
 
     public void play(View view){
+        //add database code which will reduce the number of potions based on selectedPotion int
+
         Intent intent = new Intent(this, PlayActivity.class);
         startActivity(intent);
         overridePendingTransition(0, 0);
--- app/src/main/java/com/example/flyingpotato/ShopActivity.java
+++ app/src/main/java/com/example/flyingpotato/ShopActivity.java
@@ -8,6 +8,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -23,6 +24,7 @@ public class ShopActivity extends AppCompatActivity {
     TextView gold;
     SharedPreferences pref;
     private List<Users> users;
+    private Users user;
     private DatabaseReference database;
 
     @Override
@@ -50,7 +52,7 @@ public class ShopActivity extends AppCompatActivity {
                 users.clear();
 
                 for (DataSnapshot data: dataSnapshot.getChildren()) {
-                    Users user = data.getValue(Users.class);
+                    user = data.getValue(Users.class);
                     users.add(user);
                 }
                 for(Users user: users){
@@ -66,6 +68,54 @@ public class ShopActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void buyObstruction(View view){
+        for(Users user: users){
+            if(user.getName().compareTo(pref.getString("username", "")) == 0){
+                if (user.getCash()>=10) {
+                    user.setCash(user.getCash() - 10);
+                    user.setLessObs(user.getLessObs() + 1);
+                    //add code to deduct from DB here
+                    gold.setText(String.format("%.02f Gold", user.getCash()));
+                    Toast.makeText(this, "Bought 1 Obstruction Potion for 10 Gold!", Toast.LENGTH_LONG).show();
+                }else{
+                        Toast.makeText(this, "You don't have enough money...", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    public void buyMultiplier(View view){
+        for(Users user: users){
+            if(user.getName().compareTo(pref.getString("username", "")) == 0){
+                if (user.getCash()>=15) {
+                    user.setCash(user.getCash() - 15);
+                    user.setGoldMulti(user.getGoldMulti() + 1);
+                    //add code to deduct from DB here
+                    gold.setText(String.format("%.02f Gold", user.getCash()));
+                    Toast.makeText(this, "Bought 1 Money Multiplier Potion for 15 Gold!", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(this, "You don't have enough money...", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    public void buySlow(View view){
+        for(Users user: users){
+            if(user.getName().compareTo(pref.getString("username", "")) == 0){
+                if (user.getCash()>=20) {
+                    user.setCash(user.getCash() - 20);
+                    user.setLowSpeed(user.getLowSpeed() + 1);
+                    //add code to deduct from DB here
+                    gold.setText(String.format("%.02f Gold", user.getCash()));
+                    Toast.makeText(this, "Bought 1 Slow Mo Potion for 20 Gold!", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(this, "You don't have enough money...", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 
     public void back(View view){
