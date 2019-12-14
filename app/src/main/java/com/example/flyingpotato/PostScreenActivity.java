39b1f78aba44a2805ece778799400767af828bc2
Krytosys <47263260+Krytosys@users.noreply.github.com>
2019-12-15 00:14:53 +0800

    gold+highscore

--- app/src/main/java/com/example/flyingpotato/PostScreenActivity.java
+++ app/src/main/java/com/example/flyingpotato/PostScreenActivity.java
@@ -1,5 +1,6 @@
 package com.example.flyingpotato;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,11 +11,21 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class PostScreenActivity extends AppCompatActivity {
 
     TextView congrats, score, gold;
     Button play;
     LinearLayout thingo;
+    Users user, currUser;
+    ArrayList<Users> users;
+    private DatabaseReference database;
 
     SharedPreferences pref, pref2;
 
@@ -42,6 +53,27 @@ public class PostScreenActivity extends AppCompatActivity {
 
         score.setText(pref2.getInt("level", 0));
 
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot data: dataSnapshot.getChildren()) {
+                    user = data.getValue(Users.class);
+                    users.add(user);
+                }
+                for(Users user: users){
+                    if(user.getName().compareTo(pref.getString("username", "")) == 0){
+                       currUser = user;
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
         if(!result){
             thingo.removeView(play);
             congrats.setText("Game Over!");
@@ -52,13 +84,20 @@ public class PostScreenActivity extends AppCompatActivity {
         } else {
             if(pref2.getInt("powerup", 0) != 2){
                 gold.setText("You Earned 20 Gold!");
-                //add gold to user
+                currUser.setCash(currUser.getCash()+20);
+                database.child(currUser.getId()).setValue(currUser);
             } else {
                 gold.setText("You Earned 50 Gold!");
+                currUser.setCash(currUser.getCash()+50);
+                database.child(currUser.getId()).setValue(currUser);
                 //add gold to user
             }
 
             //if score > highest score, then replace high score
+            if(Integer.parseInt(score.getText().toString().trim()) > currUser.getHighscore()){
+                currUser.setHighscore(Integer.parseInt(score.getText().toString().trim()));
+                database.child(currUser.getId()).setValue(currUser);
+            }
         }
     }
 
